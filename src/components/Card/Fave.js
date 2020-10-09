fe188a213a702af2cc6e520dc2bb1c9352e7b9c4
Zane <zanebgriffin@gmail.com>
2020-10-08 23:55:32 -0500

    edited style and icons

--- src/components/Card/Fave.js
+++ src/components/Card/Fave.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
 
 const Fave = (props) => {
     // console.log('fave', props)
-
+    const [isFave, setIsFave] = useState(false)
     const handleClick = (event) => {
         props.handleFaveClick()
-        
+        setIsFave(!isFave)
     }
 
     return (
         <>
-            <FontAwesomeIcon icon={faStar} size='2x' color='red' onClick={handleClick} className='fave'/>
+            {isFave === false ? <FontAwesomeIcon icon={faStar} size='2x' color='red' onClick={handleClick} className='fave'/> : <FontAwesomeIcon icon={faStarRegular} size='2x' color='red' onClick={handleClick} className='fave'/>}
         </>
     )
 
